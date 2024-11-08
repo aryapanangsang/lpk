a0589a4bf06abb518df25b32ea5ac314c26c0466
aryapanangsang <farizrizal29@gmail.com>
2024-11-08 14:56:19 +0700

    Refactor View User1

--- koneksi.php
+++ koneksi.php
@@ -2,12 +2,14 @@
 // Konfigurasi Data Base
 $hostname ="localhost";
 // $username ="id19664824_lpkpbi";
-// $username ="u1482559_lpk-pbi";
-// $password ="fBi0YC~*zzd9330m";
-// $db = "u1482559_daftar-pbi";
-$username = "root";
-$password = "";
-$db = "test";
+$username ="u1482559_lpk-pbi";
+$password ="fBi0YC~*zzd9330m";
+$db = "u1482559_web";
+
+// Local
+// $username = "root";
+// $password = "";
+// $db = "test";
 
 $con=mysqli_connect($hostname,$username,$password,$db);
 
