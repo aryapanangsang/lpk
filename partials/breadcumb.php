4fbc78230265ea82946db6ea322a673e636f289f
aryapanangsang <farizrizal29@gmail.com>
2024-11-08 14:15:26 +0700

    Refactor View User1

---
+++ partials/breadcumb.php
@@ -0,0 +1,17 @@
+<style>
+    a{
+        text-decoration:none;
+        color:black;
+    }
+    a:hover{
+        color: brown;
+    }
+</style>
+<nav aria-label="breadcrumb" class="main-breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="home.php">Home</a></li>
+        <li class="breadcrumb-item"><a href="profile_pelamar.php">Profile Saya</a></li>
+        <li class="breadcrumb-item"><a href="lowongan.php">Lowongan</a></li>
+        <li class="breadcrumb-item"><a href="logout.php">Logout</a></li>
+    </ol>
+</nav>
